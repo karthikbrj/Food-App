590dfa7159f0263fe8bfd5e95cb4e99bea30845a
karthik <karthikbncet@gmail.com>
2024-05-21 11:34:54 +0530

    Initial commit

---
+++ front-end/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Header from "../../components/Navbar/Header/Header.jsx";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <ExploreMenu />
+    </div>
+  );
+};
+
+export default Home;
