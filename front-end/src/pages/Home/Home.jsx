3272b1fc60004c73d183583fcbf382d91dedefbe
karthik <karthikbncet@gmail.com>
2024-05-22 11:34:47 +0530

    added filter on menu items

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Navbar/Header/Header.jsx";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
 const Home = () => {
+  const [category,setCategory] = useState("All");
+
   return (
     <div>
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
